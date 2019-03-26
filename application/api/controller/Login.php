942905c110f75947a6f596b371a6da862264a7d6
wpf244 <244761939@qq.com>
2019-03-26 15:38:06 +0800

    0.0

--- application/api/controller/Login.php
+++ application/api/controller/Login.php
@@ -1,6 +1,9 @@
 <?php
 namespace app\api\controller;
 
+use think\Request;
+
+
 
 class Login extends BaseApi
 {
@@ -87,11 +90,11 @@ class Login extends BaseApi
             $data['phone']=$phone;
             $data['code']=$code;
             $data['time']=time();
-            $re=\db("code")->where("phone='$phone'")->find();
+            $re=\db("sms_code")->where("phone='$phone'")->find();
             if($re){
-                $del=db("code")->where("phone='$phone'")->delete();
+                $del=db("sms_code")->where("phone='$phone'")->delete();
             }
-            $rea=db("code")->insert($data);
+            $rea=db("sms_code")->insert($data);
             Post($phone,$code);
             if($rea){
                 $arr=[
@@ -110,4 +113,222 @@ class Login extends BaseApi
         }
         echo json_encode($arr);
     }
+    /**
+    * 注册
+    *
+    * @return void
+    */
+    public function save()
+    {
+        $uid=Request::instance()->header("uid");
+        $level=input("level");
+        $data=input("post.");
+        $data['status']=1;
+        $data['level']=1;
+        $phone=input("phone");
+        $reu=db("user")->where("phone",$phone)->find();
+        $code=input("code");
+        $re=db("sms_code")->where(['phone'=>$phone,'code'=>$code])->find();
+        if($re){
+            $time=$re['time'];
+            $times=time();
+            $c_time=($times-$time);
+            if($c_time < 300){
+                db("sms_code")->where("id",$re['id'])->delete();
+                if($reu){
+                    $arr=[
+                        'error_code'=>5,
+                        'msg'=>'此手机号码已注册',
+                        'data'=>''
+                    ];
+                }else{
+                    $user=db("user")->where("uid",$uid)->find();
+                    if($user){
+                    unset($data['code']);
+                    if($level == 2){
+                        
+                        $res=db("user")->where("uid",$uid)->update($data);
+                        $datas['u_id']=$uid;
+                        $datas['u_phone']=$phone;
+                        $datas['u_level']=$level;
+                        $datas['u_time']=time();
+                        db("user_apply")->insert($datas);
+                        if($res){
+                                $arr=[
+                                    'error_code'=>0,
+                                    'msg'=>'注册成功',
+                                    'data'=>''
+                                ]; 
+                                }else{
+                                $arr=[
+                                    'error_code'=>4,
+                                    'msg'=>'注册失败',
+                                    'data'=>''
+                                ]; 
+                                }
+                                
+                            }else{
+                            
+                                $res=db("user")->where("uid",$uid)->update($data);
+                                if($res){
+                                    $arr=[
+                                        'error_code'=>0,
+                                        'msg'=>'注册成功',
+                                        'data'=>''
+                                    ]; 
+                                }else{
+                                    $arr=[
+                                        'error_code'=>4,
+                                        'msg'=>'注册失败',
+                                        'data'=>''
+                                    ]; 
+                                }
+                            }
+                            }else{
+                                $arr=[
+                                'error_code'=>3,
+                                'msg'=>'授权失败',
+                                'data'=>''
+                                ]; 
+                            }
+                        }
+            }else{
+                $arr=[
+                    'error_code'=>2,
+                    'msg'=>'验证码已失效',
+                    'data'=>''
+                ]; 
+            }
+        }else{
+            $arr=[
+                'error_code'=>1,
+                'msg'=>'验证码错误',
+                'data'=>''
+            ];
+        }
+        
+        echo json_encode($arr);
+    }
+    /**
+    *  忘记密码获取验证码
+    *
+    * @return void
+    */
+    public function forget_get_code()
+    {
+        $phone=input('phone');
+        $re=db('user')->where("phone=$phone")->find();
+        if($re){
+            $code =mt_rand(100000,999999);       
+            $data['phone']=$phone;
+            $data['code']=$code;
+            $data['time']=time();
+            $re=\db("sms_code")->where("phone='$phone'")->find();
+            if($re){
+                $del=db("sms_code")->where("phone='$phone'")->delete();
+            }
+            $rea=db("sms_code")->insert($data);
+            Post($phone,$code);
+            if($rea){
+                $arr=[
+                    'error_code'=>0,
+                    'msg'=>'发送成功',
+                    'data'=>''
+                ];
+            }else{
+                $arr=[
+                    'error_code'=>2,
+                    'msg'=>'发送失败',
+                    'data'=>''
+                ];
+            }
+           
+        }else{
+            $arr=[
+                'error_code'=>1,
+                'msg'=>'此手机号未注册',
+                'data'=>""
+            ];
+            
+        }
+        echo json_encode($arr);
+    }
+    /**
+    * 修改密码
+    *
+    * @return void
+    */
+    public function usave()
+    {
+        $code=input("code");
+        $phone=input("phone");
+        $re=db("sms_code")->where(['phone'=>$phone,'code'=>$code])->find();
+        if($re){
+            $time=$re['time'];
+            $times=time();
+            $c_time=($times-$time);
+            if($c_time < 300){
+                db("sms_code")->where("id",$re['id'])->delete();
+                $user=db("user")->where("phone",$phone)->find();
+                if($user){
+                    $data['pwd']=input("pwd");
+                    db("user")->where("phone",$phone)->update($data);
+                   
+                    $arr=[
+                        'error_code'=>0,
+                        'msg'=>'修改成功',
+                        'data'=>''
+                    ]; 
+                    
+                }else{
+                    $arr=[
+                        'error_code'=>3,
+                        'msg'=>'此手机号码未注册',
+                        'data'=>''
+                    ]; 
+                }
+            }else{
+                $arr=[
+                    'error_code'=>2,
+                    'msg'=>'验证码已失效',
+                    'data'=>''
+                ]; 
+            }
+        }else{
+            $arr=[
+                'error_code'=>1,
+                'msg'=>'验证码错误',
+                'data'=>''
+            ];
+        }
+        
+        echo json_encode($arr);
+
+    }
+    /**
+    * 登录
+    *
+    * @return void
+    */
+    public function index()
+    {
+        $phone=input("phone");
+        $pwd=input("pwd");
+        $re=db("user")->where(["phone"=>$phone,"pwd"=>$pwd])->find();
+        if($re){
+            $arr=[
+                'error_code'=>0,
+                'msg'=>'登录成功',
+                'data'=>['uid'=>$re['uid']]
+            ]; 
+        }else{
+            $arr=[
+                'error_code'=>1,
+                'msg'=>'账号或密码错误',
+                'data'=>[]
+            ]; 
+        }
+        echo json_encode($arr);
+    }
+   
 }
